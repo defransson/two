5d641c7f4f5c53afe66c5842a63ebf9f2ae5f89f
defransson <goran_fransson@hotmail.com>
2015-09-17 10:15:49 +0200

    Imageupload

--- js/twoscript.js
+++ js/twoscript.js
@@ -113,6 +113,9 @@ function init_click(){
             $("#registerconfirm").click(function(){
                     add_user();
              });
+             $("#avatar").click(function(){
+                    getImage();
+             });
 }
 function grabtask(){
 	
@@ -244,3 +247,43 @@ function onDeviceReady() {
         
 
     }
+    function getImage() {
+            // Retrieve image file location from specified source
+            navigator.camera.getPicture(uploadPhoto, function(message) {
+            alert('get picture failed');
+        },{
+            quality: 50, 
+            destinationType: navigator.camera.DestinationType.FILE_URI,
+            sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+        }
+            );
+ 
+        }
+ 
+        function uploadPhoto(imageURI) {
+            var options = new FileUploadOptions();
+            options.fileKey="file";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+ 
+            var params = new Object();
+            params.value1 = "test";
+            params.value2 = "param";
+ 
+            options.params = params;
+            options.chunkedMode = false;
+ 
+            var ft = new FileTransfer();
+            ft.upload(imageURI, "https:/www.kubary.se/two/php/upload.php", win, fail, options);
+        }
+ 
+        function win(r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+            alert(r.response);
+        }
+ 
+        function fail(error) {
+            alert("An error has occurred: Code = " = error.code);
+        }
